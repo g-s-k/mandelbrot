5ccf0244423763b9343eeda9dfe2265b3fc6588a
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-12 11:53:05 -0400

    tenth listing: a single-threaded program

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,26 @@ use image::ColorType;
 use image::png::PNGEncoder;
 
 fn main() {
-    println!("This is a mandelbrot calculator, per the O'Reilly book.");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT");
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(0);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
