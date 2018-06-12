3e12d1785abbd6d38c90523a568c742ddbb3553f
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-12 10:50:09 -0400

    second listing: signature change, addition-per-loop

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ fn main() {
     println!("This is a mandelbrot calculator, per the O'Reilly book.");
 }
 
-fn square_loop(mut x: f64) {
+fn square_loop(c: f64) {
+    let mut x = 0.;
     loop {
         x *= x;
+        x += c;
     }
 }
