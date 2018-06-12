bba14ef565762892d47cb2be5eaa6f05653ff153
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-12 11:15:52 -0400

    fifth listing: fn to parse pairs from string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 
+use std::str::FromStr;
 use num::Complex;
 
 fn main() {
@@ -7,7 +8,7 @@ fn main() {
 }
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
-    let mut z = Complex{ re: 0.0, im: 0.0 };
+    let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         z *= z;
         z += c;
@@ -18,4 +19,28 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     }
 
     None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            if let (Ok(l), Ok(r)) = (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                Some((l, r))
+            } else {
+                None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x,", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
