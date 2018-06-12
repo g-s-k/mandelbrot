6d71cf59a182ae19b58d845a40b5002a3deb356c
George Kaplan <gkaplan@nu-tek-optics.com>
2018-06-12 11:38:22 -0400

    eighth listing: render fn

--- src/main.rs
+++ src/main.rs
@@ -94,3 +94,23 @@ fn test_pixel_to_point() {
         Complex { re: -0.5, im: -0.5 }
     );
 }
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = if let Some(count) = escape_time(point, 255) {
+                255 - count as u8
+            } else {
+                0
+            };
+        }
+    }
+}
